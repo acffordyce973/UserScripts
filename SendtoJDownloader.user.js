b24fcbccf5ebb011df82b5ee595244cebdd9608e
acffordyce973 <acffordyce973@users.noreply.github.com>
2025-10-24 19:06:40 +0100

    Update version and improve error handling

--- SendtoJDownloader.user.js
+++ SendtoJDownloader.user.js
@@ -3,10 +3,11 @@
 // @namespace    acf.me.uk
 // @description  Send URLs to JDownloader via the right click context menu. TamperMonkey only!
 // @author       ACF
-// @version      1.0
+// @version      0.2
 // @updateURL    https://github.com/acffordyce973/UserScripts/raw/refs/heads/main/SendtoJDownloader.user.js
 // @include      *
 // @grant        GM_registerMenuCommand
+// @grant        GM_notification
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
@@ -15,32 +16,39 @@
     'use strict';
 
     function sendToJDownloader(downloadUrl) {
-        var jdownloaderUrl = 'http://127.0.0.1:9666/flash/add?urls='+downloadUrl;
+        var jdownloaderUrl = 'http://127.0.0.1:9666/flash/add?urls=' + downloadUrl;
 
         GM.xmlHttpRequest({
             method: 'POST',
             url: jdownloaderUrl,
-            headers : {
+            headers: {
                 Referer: "http://localhost/"
             },
-            data: 'urls='+ downloadUrl,
+            data: 'urls=' + downloadUrl,
             onload: function(response) {
-                console.log('URL sent to JDownloader: '+downloadUrl);
+                GM_notification({
+                    text: downloadUrl,
+                    title: "Link Sent to JDownloader",
+                    url: "https://jdownloader.org/",
+                    onclick: (event) => {
+                        console.log("Notification was clicked.");
+                    },
+                });
             },
             onerror: function(error) {
-                console.error('Error sending URL to JDownloader:', error);
+                alert(`Error sending URL to JDownloader: ${error}`);
             }
         });
     }
 
     let clickedEl = null;
 
-    document.addEventListener("contextmenu", function(event){
+    document.addEventListener("contextmenu", function(event) {
         clickedEl = event.target;
     });
 
     GM_registerMenuCommand("Grab Link", () => {
-        if(clickedEl){
+        if (clickedEl) {
             console.log(`the clicked element is :`)
             console.log(clickedEl)
             const target = clickedEl.closest("a");
