3fe2eab032b77cace92ad3e32ac7a315612fdb7e
acffordyce973 <acffordyce973@users.noreply.github.com>
2025-10-27 18:58:50 +0000

    Add download and update URLs to metadata
    Added download and update URLs for the script.

--- SendtoJDownloader.user.js
+++ SendtoJDownloader.user.js
@@ -4,6 +4,7 @@
 // @description  Send URLs to JDownloader via the right click context menu. TamperMonkey only!
 // @author       ACF
 // @version      0.3
+// @downloadURL  https://github.com/acffordyce973/UserScripts/raw/refs/heads/main/SendtoJDownloader.user.js
 // @updateURL    https://github.com/acffordyce973/UserScripts/raw/refs/heads/main/SendtoJDownloader.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=jdownloader.org
 // @include      *
